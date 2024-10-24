4f8330b7e1ce090083733d025f0bc1db7ddbbea7
KiritoKitto <88983572+KiritoKitto@users.noreply.github.com>
2024-10-24 15:07:40 +0200

    Try 2

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,33 +1,30 @@
-// Funzione per caricare il tema
-function loadTheme(theme) {
-    const themeLink = document.createElement("link");
-    themeLink.id = "theme-link"; // Assegna un ID per il link
-    themeLink.rel = "stylesheet";
-    themeLink.href = `{{ 'css/${theme}.css' | relative_url }}`; // Usa il tema passato come argomento
-    document.head.appendChild(themeLink); // Aggiungi il link al <head>
-}
-
 // Funzione per cambiare il tema
 function switchTheme(event) {
     event.preventDefault();  // Evita il comportamento predefinito del link
 
-    const currentTheme = document.getElementById("theme-link").getAttribute("href");
+    const themeLink = document.getElementById("theme-link");
+    const currentTheme = themeLink.getAttribute("href");
 
     // Cambia il tema e salva la scelta nel localStorage
-    if (currentTheme.includes("css/dark.css")) {
-        loadTheme("light"); // Carica il tema chiaro
-        localStorage.setItem("theme", "light");
-    } else {
-        loadTheme("dark"); // Carica il tema scuro
+    if (currentTheme === "css/light.css") {
+        themeLink.setAttribute("href", "/css/dark.css");
         localStorage.setItem("theme", "dark");
+    } else {
+        themeLink.setAttribute("href", "light.css");
+        localStorage.setItem("theme", "/css/light");
     }
 }
 
 // Al caricamento della pagina, controlla il tema nel localStorage
 window.addEventListener("load", function() {
-    const savedTheme = localStorage.getItem("theme") || "dark"; // Imposta il tema di default a "dark"
+    const savedTheme = localStorage.getItem("theme");
 
-    loadTheme(savedTheme); // Carica il tema salvato o quello di default
+    // Se c'è un tema salvato, applicalo; altrimenti imposta il tema di default (light.css)
+    if (savedTheme) {
+        document.getElementById("theme-link").setAttribute("href", "/css/" + savedTheme + ".css");
+    } else {
+        document.getElementById("theme-link").setAttribute("href", "/css/dark.css");
+    }
 });
 
 // Aggiungi un event listener al link per cambiare tema
