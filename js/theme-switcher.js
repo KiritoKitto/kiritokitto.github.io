c96367df931508514756275700e9668a1c29f8ed
KiritoKitto <88983572+KiritoKitto@users.noreply.github.com>
2024-10-24 15:01:00 +0200

    Theme Fixer

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,32 +1,33 @@
+// Funzione per caricare il tema
+function loadTheme(theme) {
+    const themeLink = document.createElement("link");
+    themeLink.id = "theme-link"; // Assegna un ID per il link
+    themeLink.rel = "stylesheet";
+    themeLink.href = `{{ '/css/${theme}.css' | relative_url }}`; // Usa il tema passato come argomento
+    document.head.appendChild(themeLink); // Aggiungi il link al <head>
+}
+
 // Funzione per cambiare il tema
 function switchTheme(event) {
     event.preventDefault();  // Evita il comportamento predefinito del link
-    const themeLink = document.getElementById("theme-link");
-    const currentTheme = themeLink.getAttribute("href");
+
+    const currentTheme = document.getElementById("theme-link").getAttribute("href");
 
     // Cambia il tema e salva la scelta nel localStorage
-    if (currentTheme === "css/dark.css") {
-        themeLink.setAttribute("href", "css/light.css");
+    if (currentTheme.includes("dark.css")) {
+        loadTheme("light"); // Carica il tema chiaro
         localStorage.setItem("theme", "light");
     } else {
-        themeLink.setAttribute("href", "css/dark.css");
+        loadTheme("dark"); // Carica il tema scuro
         localStorage.setItem("theme", "dark");
     }
 }
 
 // Al caricamento della pagina, controlla il tema nel localStorage
 window.addEventListener("load", function() {
-    const savedTheme = localStorage.getItem("theme");
+    const savedTheme = localStorage.getItem("theme") || "dark"; // Imposta il tema di default a "dark"
 
-    // Se c'è un tema salvato
-    if (savedTheme === "dark") {
-        document.getElementById("theme-link").setAttribute("href", "css/dark.css");
-    } else if (savedTheme === "light") {
-        document.getElementById("theme-link").setAttribute("href", "css/light.css");
-    } else {
-        document.getElementById("theme-link").setAttribute("href", "css/dark.css");
-        localStorage.setItem("theme", "dark"); // Imposta light come default per i nuovi visitatori
-    }
+    loadTheme(savedTheme); // Carica il tema salvato o quello di default
 });
 
 // Aggiungi un event listener al link per cambiare tema
