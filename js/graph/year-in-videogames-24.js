97659d2e46bde880b14f7bd285d1510a3b744f72
KiritoKitto <88983572+KiritoKitto@users.noreply.github.com>
2024-11-12 22:51:53 +0100

    change graph

--- js/graph/year-in-videogames-24.js
+++ js/graph/year-in-videogames-24.js
@@ -14,109 +14,63 @@ function drawVisualization() {
     const colorPrimary = rootStyles.getPropertyValue('--color-primary').trim();
     const colorSecondary = rootStyles.getPropertyValue('--color-secondary').trim();
     
-    drawCompletedGames();
-    drawPercentage();
-}
-
-function drawCompletedGames() {
     var data = google.visualization.arrayToDataTable([
         ['Year', 'Completed', 'Not Completed'],
         ['2024', 23, 12],
         ['2023', 39, 17],
-        ['2022', 48, 25]
-    ]);
+        ['2022', 48, 25]]);
 
     var dynamicHeight = data.getNumberOfRows() * (50 + (3 * data.getNumberOfRows()));
-
-    var options = {
-        width: '100%',
-        height: '100%',
-        title: '',
-        vAxis: {
-            title: '',
-            textStyle: { fontName: font, color: colorSecondary }
-        },
-        hAxis: {
-            title: '',
-            textStyle: { fontName: font, color: colorSecondary },
-            gridlines: { color: graphbaseprimary },
-            minorGridlines: { count: 0 }
-        },
-        legend: {
-            position: 'bottom',
-            alignment: 'center',
-            maxLines: 10,
-            textStyle: { fontName: font, color: colorSecondary }
-        },
+    var options = { width: '100%', height: '100%',title: '',
+        vAxis: {textStyle: { fontName: font, color: colorSecondary }},
+        hAxis: {textStyle: { fontName: font, color: colorSecondary },gridlines: { color: graphbaseprimary },minorGridlines: { count: 0 }},
+        legend: {textStyle: { fontName: font, color: colorSecondary }, position: 'bottom', alignment: 'center', maxLines: 10},
         series: {
             0: { color: graphhighlightprimary },
             1: { color: graphbaseprimary }
         },
         backgroundColor: { fill: 'transparent' },
         isStacked: true,
-        chartArea: {
-            width: '100%',
-            height: '100%',
+        chartArea: {width: '100%',height: '100%',
             bottom: 60,
             left: 35,
-        }
-    };
+    }};
 
     document.getElementById('completed-games').style.height = `${dynamicHeight}px`;
-
     var chart = new google.visualization.BarChart(document.getElementById('completed-games'));
     chart.draw(data, options);
-}
 
-function drawPercentage() {
+    // ----------------------
+    // DIVISORE -------------
+    // ----------------------
+
     var data = google.visualization.arrayToDataTable([
-        ['Year', 'Completed', 'Not Completed'],
-        ['2024', 23, 12],
-        ['2023', 39, 17],
-        ['2022', 48, 25]
-    ]);
+        ['Year', 'Completed', 'Retired', 'Shelved', 'Abandoned'],
+        ['2024', 23,4,7,1],
+        ['2023', 39,6,7,4],
+        ['2022', 48,11,8,6]]);
 
     var dynamicHeight = data.getNumberOfRows() * (50 + (3 * data.getNumberOfRows()));
-
-    var options = {
-        width: '100%',
-        height: '100%',
-        title: '',
-        vAxis: {
-            title: '',
-            textStyle: { fontName: font, color: colorSecondary }
-        },
-        hAxis: {
-            title: '',
-            textStyle: { fontName: font, color: colorSecondary },
-            gridlines: { color: graphbaseprimary },
-            minorGridlines: { count: 0 }
-        },
-        legend: {
-            position: 'bottom',
-            alignment: 'center',
-            maxLines: 10,
-            textStyle: { fontName: font, color: colorSecondary }
-        },
+    var options = { width: '100%', height: '100%',title: '',
+        vAxis: {textStyle: { fontName: font, color: colorSecondary }},
+        hAxis: {textStyle: { fontName: font, color: colorSecondary },gridlines: { color: graphbaseprimary },minorGridlines: { count: 0 }},
+        legend: {textStyle: { fontName: font, color: colorSecondary }, position: 'bottom', alignment: 'center', maxLines: 10},
         series: {
             0: { color: graphhighlightprimary },
-            1: { color: graphbaseprimary }
+            1: { color: graphbaseprimary },
+            2: { color: graphbasesecondary },
+            3: { color: graphbaseprimary }
         },
         backgroundColor: { fill: 'transparent' },
-        isStacked: true,
-        chartArea: {
-            width: '100%',
-            height: '100%',
+        isStacked: 'percent',
+        chartArea: {width: '100%',height: '100%',
             bottom: 60,
             left: 35,
-        }
-    };
+    }};
 
     document.getElementById('percentage').style.height = `${dynamicHeight}px`;
-
     var chart = new google.visualization.BarChart(document.getElementById('percentage'));
     chart.draw(data, options);
 }
-
 window.addEventListener('resize', drawVisualization);
 document.getElementById("theme-link-switcher").addEventListener("click", drawVisualization);
