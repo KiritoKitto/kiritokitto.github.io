fa615ae619a4bc8d354377f800d01ed98802e3ca
KiritoKitto <phonemix.shawn@gmail.com>
2025-12-07 22:10:20 +0100

    ReGen

--- _site/js/list/photomod.js
+++ _site/js/list/photomod.js
@@ -2,13 +2,13 @@ const seasons = [
     {
     title: "Metal Gear Solid: Delta",
     date: "September 2025",
-    path: "Game Photography/metal gear solid delta",
+    path: "/photomode/metal gear solid delta",
     banner: "https://pub-6a75fc66f72e4a47a683fa56714dc173.r2.dev/Website/Photomod/Metal%20Gear%20Solid%20Delta/Image08.jpg"
 },
     {
     title: "Hollow Knight: Silksong",
     date: "September 2025",
-    path: "Game Photography/hollow knight silksong",
-    banner: "https://pub-6a75fc66f72e4a47a683fa56714dc173.r2.dev/Website/Photomod/Hollow Knight Silksong/Image10.jpg"
+    path: "/photomode/hollow knight silksong",
+    banner: "https://pub-6a75fc66f72e4a47a683fa56714dc173.r2.dev/Website/Photomod/Hollow Knight Silksong/Image02.jpg"
 },
 ];
