136de879a1be10fa25a62556fce5c9a26bcbc6b4
KiritoKitto <phonemix.shawn@gmail.com>
2025-12-07 21:37:59 +0100

    update

---
+++ _site/js/list/trails.js
@@ -0,0 +1,94 @@
+const seasons = [
+  {
+    title: "Season 4 - RainFall",
+    trails: [
+      {
+        title: "Milano",
+        date: "Dec 18",
+        search: "Website/Into%20the%20Wilds/s4/roma",
+        banner: "https://pub-6a75fc66f72e4a47a683fa56714dc173.r2.dev/Website/Into%20the%20Wilds/s4/milano/m2.jpg"
+      },
+      {
+        title: "Milano",
+        date: "Oct 18",
+        search: "Website/Into%20the%20Wilds/s4/milano",
+        banner: "https://pub-6a75fc66f72e4a47a683fa56714dc173.r2.dev/Website/Into%20the%20Wilds/s4/milano/m2.jpg"
+      }
+    ]
+  },
+  
+  {
+    title: "Season 3 - OK",
+    trails: [
+      {
+        title: "Milano",
+        date: "Dec 18",
+        order: "1",
+        search: "Website/Into%20the%20Wilds/s4/barberino",
+        banner: "https://pub-6a75fc66f72e4a47a683fa56714dc173.r2.dev/Website/Into%20the%20Wilds/s4/milano/m2.jpg"
+      }
+    ]
+  },
+  
+  {
+    title: "Oltre il Firmamento 2",
+    trails: [
+      {
+        title: "La dualità della Sacra Città Celeste",
+        subtitle: "Siena",
+        date: "Aprile 2024",
+        search: "",
+        banner: ""
+      },
+      {
+        title: "L'Esotica Città dell'Oro",
+        subtitle: "Arezzo",
+        date: "Aprile 2023",
+        search: "",
+        banner: ""
+      },
+      {
+        title: "Le 7 + 2 Meraviglie di Cavour",
+        subtitle: "Bologna",
+        date: "Gennaio 2023",
+        search: "",
+        banner: ""
+      }
+    ]
+  },
+  
+  {
+    title: "Oltre il Firmamento",
+    trails: [
+      {
+        title: "Il Tragitto con Attesa",
+        subtitle: "Lucca",
+        date: "Ottobre  2022",
+        search: "",
+        banner: ""
+      },
+      {
+        title: "La Via della Salvezza",
+        subtitle: "Chiusi",
+        date: "Agosto 2022",
+        search: "",
+        banner: ""
+      },
+      {
+        title: "Il Viaggio della Speranza",
+        subtitle: "Prato",
+        date: "Agosto 2022",
+        search: "",
+        banner: ""
+      },
+      {
+        title: "Il Cammino senza Fermate",
+        subtitle: "Firenze",
+        date: "Luglio 2022",
+        search: "",
+        banner: "https://pub-6a75fc66f72e4a47a683fa56714dc173.r2.dev/Website/Into%20the%20Wilds/2022-08-20%20-%20Chiusi/Image01.jpg"
+      },
+    ]
+  }
+  
+];
